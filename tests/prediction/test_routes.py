6e5598b980415ef7a867f83a8c58b8a38e43d4ea
Mukund Iyer <msiyer@student.ubc.ca>
2024-04-27 12:24:01 -0700

    test: change structure of test

--- tests/prediction/test_routes.py
+++ tests/prediction/test_routes.py
@@ -1,25 +1,50 @@
 # test_routes.py
+from unittest.mock import Mock, patch
 from flask_testing import TestCase
+from PIL import Image
 from app import app  # Adjust this import to fit your project structure.
+
 import os
 import pytest
+import torch
+import tempfile
 
+
+from src.image_preprocessing import Transformation
+from src.model_inference import Inference
+
+@pytest.fixture
+def sample_image():
+    return Image.new('RGB', (256, 256))
+
+@pytest.fixture
+def mock_model():
+    model = Mock()
+    model.eval.return_value = None
+    model.return_value = torch.tensor([[0.7, 0.3]])
+    return model
+
+@pytest.fixture
+def inference_instance(mock_model, mock_transformation):
+    with patch('torch.load', return_value=mock_model):
+        return Inference(model_path="dummy/path/model.pth", transforms=mock_transformation)
+    
 class TestFlaskRoutes(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
+    @pytest.mark.parametrize("inference_instance, sample_image", [(inference_instance, sample_image)])
     def test_predict(self):
-        path_to_image = os.path.join('tests', 'prediction','images', 'image_1.jpeg')
-        with open(path_to_image, 'rb') as img:
-            response = self.client.post(
-                '/predict',
-                data={'file': (img, 'test_image.jpg')},
-                content_type='multipart/form-data'
-            )
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions based on your application's response structure.
+        with patch('PIL.Image.open', return_value=sample_image):
+            with patch('torch.load', return_value=inference_instance):
+                response = self.client.post(
+                    '/predict',
+                    data={'file': (sample_image, 'test_image.jpg')},
+                    content_type='multipart/form-data'
+                )
 
+        self.assertEqual(response.status_code, 200)
 # Run the tests
 if __name__ == "__main__":
     pytest.main()
